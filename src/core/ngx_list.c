f331ace20a2b074d239eca27f8d76954b286f3a7
duwei19961021 <duwei19961021@163.com>
2021-03-21 23:21:52 +0800

    nginx单向链表：代码注释

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -14,12 +14,12 @@ ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
     ngx_list_t  *list;
 
-    list = ngx_palloc(pool, sizeof(ngx_list_t));
+    list = ngx_palloc(pool, sizeof(ngx_list_t));    // 在small链表上给链表的管理结构(ngx_list_t)分配内存
     if (list == NULL) {
         return NULL;
     }
 
-    if (ngx_list_init(list, pool, n, size) != NGX_OK) {
+    if (ngx_list_init(list, pool, n, size) != NGX_OK) { // 初始化数据区域
         return NULL;
     }
 
@@ -33,18 +33,18 @@ ngx_list_push(ngx_list_t *l)
     void             *elt;
     ngx_list_part_t  *last;
 
-    last = l->last;
+    last = l->last;     // 拿到最后一个元素的指针，尾插效率高
 
-    if (last->nelts == l->nalloc) {
+    if (last->nelts == l->nalloc) { // 说明空间用完了，需要扩容
 
         /* the last part is full, allocate a new list part */
 
-        last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
+        last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));    // 从内存池上给结点分配内存
         if (last == NULL) {
             return NULL;
         }
 
-        last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
+        last->elts = ngx_palloc(l->pool, l->nalloc * l->size);  // 从内存池上给新结点分配数据区的内存，每个结点的数据区域都是一样大
         if (last->elts == NULL) {
             return NULL;
         }
@@ -56,8 +56,8 @@ ngx_list_push(ngx_list_t *l)
         l->last = last;
     }
 
-    elt = (char *) last->elts + l->size * last->nelts;
+    elt = (char *) last->elts + l->size * last->nelts;  // 首地址加上已经使用的内存等于未使用内存的起始位置
     last->nelts++;
 
-    return elt;
+    return elt; // 返回分配的内存的首地址
 }
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -16,8 +16,8 @@
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
+    void             *elts;     // 数据区的首地址
+    ngx_uint_t        nelts;    //  指向最后一个使用过的元素
     ngx_list_part_t  *next;
 };
 
@@ -31,22 +31,22 @@ typedef struct {
 } ngx_list_t;
 
 
-ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
+ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);   // 创建父结点
 
 static ngx_inline ngx_int_t
 ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
-    list->part.elts = ngx_palloc(pool, n * size);
+    list->part.elts = ngx_palloc(pool, n * size);   // 分配在small还是large链表上取决于n*size
     if (list->part.elts == NULL) {
         return NGX_ERROR;
     }
 
-    list->part.nelts = 0;
-    list->part.next = NULL;
-    list->last = &list->part;
-    list->size = size;
-    list->nalloc = n;
-    list->pool = pool;
+    list->part.nelts = 0;       //  指向最后一个使用过的元素
+    list->part.next = NULL;     // 指向链表的下一个结点
+    list->last = &list->part;   // 指向当前链表的最后一个结点，便于尾插
+    list->size = size;          // 元素的大小(占用内存的字节)
+    list->nalloc = n;           // 元素个数
+    list->pool = pool;          // 指向内存池
 
     return NGX_OK;
 }
